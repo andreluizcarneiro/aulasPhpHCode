dbf152b30257660779311fed16d81e1f588b230f
AndreCastro <andreluizcarneiro@outlook.com.br>
2020-07-05 20:27:16 -0300

    Ultimos Modulos

---
+++ seguranca/exemplo01.php
@@ -0,0 +1,23 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"]==='POST'){
+$cmd = escapeshellcmd($_POST["cmd"]);
+
+var_dump($cmd);
+
+echo "<pre>";
+$comando = system("dir C:", $retorno);
+
+
+echo "</prev>";
+
+}
+
+?>
+<form method="post">
+
+	<input type="text" name="cmd">
+	<button type="submit">Enviar </button>
+
+
+</form>
---
+++ seguranca/exemplo04.php
@@ -0,0 +1,7 @@
+ <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+<form action="cadastrar.php" method="post"> 
+	<div class="g-recaptcha" data-sitekey="6LeB7K0ZAAAAAIIp42t0x0zNLt3mrQR7Ik5GusKl"></div>
+	<input type="email" name="inputEmail">
+	<button type="submit">Enviar</button>
+</form>
